ad96502aa55a01c3e0e922547b1036fd0b8502b2
Kinsey Favre <kinseytamsin@tutanota.com>
2020-01-07 22:35:13 -0600

    Reimplement Rust solution for Day 1 using futures

--- 1.rs
+++ 1.rs
@@ -1,9 +1,14 @@
-extern crate rayon;
+extern crate anyhow;
+#[macro_use]
+extern crate futures;
+extern crate tokio;
 
-use std::fs;
-use std::io;
+use std::iter;
+use std::mem;
 
-use rayon::prelude::*;
+use anyhow::{Result, Error};
+use futures::{prelude::*, channel::mpsc, stream};
+use tokio::{prelude::*, fs::File, io::BufReader};
 
 struct FuelReq { curr: i32 }
 
@@ -29,28 +34,62 @@ impl Iterator for FuelReq {
 
 fn fuel_required(mass: i32) -> i32 { mass/3 - 2 }
 
+
 fn fuel_required_recursive(mass: i32) -> i32 {
     let fuel_req_iter = FuelReq::new(mass);
     fuel_req_iter.sum()
 }
 
-fn main() -> io::Result<()> {
-    let contents = fs::read_to_string("1.txt")?;
-    let lines = contents.par_lines();
-    let masses = lines.map(|l| l.parse::<i32>().unwrap());
-    let (total_fuel, total_fuel_recursive) = masses
-        .fold(
-            || (0, 0),
-            |(acc_f, acc_fr), mass| (
-                acc_f + fuel_required(mass),
-                acc_fr + fuel_required_recursive(mass),
-            ),
-        )
-        .reduce(
-            || (0, 0),
-            |(acc_f, acc_fr), (f, fr)| (acc_f + f, acc_fr + fr),
-        );
-
+#[tokio::main(threaded_scheduler)]
+async fn main() -> Result<()> {
+    let f = File::open("1.txt").await?;
+    let buf_reader = BufReader::new(f);
+    let lines = buf_reader.lines();
+    let masses = lines.map(|line| Ok::<_, Error>(line?.parse::<i32>()?));
+    let (tx, mut rx) = mpsc::channel(0);
+    let (tx_r, mut rx_r) = mpsc::channel(0);
+    let calculation_fut =
+        masses
+            .zip(
+                stream::iter(iter::repeat(mpsc::Sender::clone(&tx)))
+                    .zip(
+                        stream::iter(iter::repeat(mpsc::Sender::clone(&tx_r)))
+                    )
+            )
+            .map(|(x, y)| Ok::<_, Error>((x?, y)))
+            .try_for_each_concurrent(
+                None,
+                |(mass, (mut tx, mut tx_r))| async move {
+                    let f = async { fuel_required(mass) };
+                    let fr = async { fuel_required_recursive(mass) };
+                    let (res, res_r) =
+                        join!(tx.send(f.await), tx_r.send(fr.await));
+                    res?;
+                    res_r?;
+                    Ok::<(), Error>(())
+                },
+            )
+            .map_err(|e| eprintln!("Error: {}", e));
+    // we never use the original Senders, only clones of them, so drop
+    // the originals
+    mem::drop(tx);
+    mem::drop(tx_r);
+    let sum_f_fut = async move {
+        let mut total_fuel = 0;
+        while let Some(f) = rx.next().await {
+            total_fuel += f;
+        }
+        total_fuel
+    };
+    let sum_fr_fut = async move {
+        let mut total_fuel_recursive = 0;
+        while let Some(fr) = rx_r.next().await {
+            total_fuel_recursive += fr;
+        }
+        total_fuel_recursive
+    };
+    tokio::spawn(calculation_fut);
+    let (total_fuel, total_fuel_recursive) = join!(sum_f_fut, sum_fr_fut);
     println!("{}", total_fuel);
     println!("{}", total_fuel_recursive);
 
